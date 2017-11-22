fb87278489e63aa929ccda298c84d305fa3b42e0
XiwenHuang <369762507@qq.com>
2017-11-22 17:49:19 +0800

    recommend组件开发完成

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 // import store from './store'
 
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
